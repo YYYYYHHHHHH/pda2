82d74e0669767284a269fa2b498ffe386f3644ff
YYYYYHHHHHH <49487798+YYYYYHHHHHH@users.noreply.github.com>
2020-08-05 11:33:52 +0800

    连接和读取皆为30

--- app/src/main/java/com/example/pda/bean/globalbean/MyOkHttpClient.java
+++ app/src/main/java/com/example/pda/bean/globalbean/MyOkHttpClient.java
@@ -13,7 +13,7 @@ public class MyOkHttpClient {
             synchronized (object) {
                 if (okHttpClient == null) {
                     okHttpClient = new OkHttpClient.Builder()
-                            .connectTimeout(10, TimeUnit.SECONDS)//设置连接超时时间
+                            .connectTimeout(30, TimeUnit.SECONDS)//设置连接超时时间
                             .readTimeout(30, TimeUnit.SECONDS) //设置读取超时时间
                             .retryOnConnectionFailure(false)
                             .build();
