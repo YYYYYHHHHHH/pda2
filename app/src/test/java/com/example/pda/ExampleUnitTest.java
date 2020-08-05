89eaf152bac31464d04007ff9561b2c33557052c
YYYYYHHHHHH <49487798+YYYYYHHHHHH@users.noreply.github.com>
2020-08-05 10:57:18 +0800

    菜单新增其他出库 - 3

--- app/src/test/java/com/example/pda/ExampleUnitTest.java
+++ app/src/test/java/com/example/pda/ExampleUnitTest.java
@@ -5,7 +5,11 @@ import com.example.pda.util.TimeUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+
+import kotlin._Assertions;
 
 import static org.junit.Assert.*;
 
@@ -25,5 +29,24 @@ public class ExampleUnitTest {
         for (String s : dateList) {
             System.out.println(s);
         }
+    }
+    @Test
+    public void text() {
+        int[] ints = {-1, -100, 3, 99};
+        rotate(ints, 2);
+        System.out.println(Arrays.toString(ints));
+    }
+    public void rotate(int[] nums, int k) {
+        int[] ints = Arrays.copyOf(nums, nums.length);
+        for (int i = 0; i < ints.length; i++) {
+            nums[(i + k) % nums.length] = ints[i];
+        }
+    }
+    public int dg(List<Integer> arrayList, int index, int num, int k) {
+        int item = arrayList.get(index);
+        int n = (num == arrayList.size()) ? arrayList.get(0) :
+                dg(arrayList, (index - k + arrayList.size()) % arrayList.size(), num + 1, k);
+        arrayList.set(index, n);
+        return item;
     }
 }
